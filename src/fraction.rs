91bf732daec95ab27d5471132bd15ae93ae5631a
jdmejiav <jdmejiav@eafit.edu.co>
2021-10-10 00:10:10 -0500

    fraction object migration

--- src/fraction.rs
+++ src/fraction.rs
@@ -4,10 +4,59 @@ pub struct Fraction {
 }
 
 impl Fraction {
-    fn new(numerator: f64, denominator: f64) -> Fraction {
+    
+    pub fn new(numerator: f64, denominator: f64) -> Fraction {
         Fraction {
             numerator,
             denominator,
         }
     }
+
+    pub fn value(&self) -> f64 {
+        self.numerator / self.denominator
+    }
+
+    pub fn get_numerator(&self) -> f64 {
+        self.numerator
+    }
+
+    pub fn get_denominator(&self) -> f64 {
+        self.denominator
+    }
+    /*
+    #[warn(dead_code)]
+    pub fn set_numerator(&mut self, numerator: f64) {
+        self.numerator = numerator;
+    }
+
+
+    #[warn(dead_code)]
+    pub fn set_denominator(&mut self, denominator: f64) {
+        self.numerator = denominator;
+    }
+    */
+}
+
+pub fn sum(f1: &Fraction, f2: &Fraction) -> Fraction {
+    let numerator = f1.get_numerator()*f2.get_denominator()+f2.get_numerator()*f1.get_denominator();
+    let denominator = f1.get_denominator()*f2.get_denominator();
+    Fraction::new(numerator, denominator)
+}
+
+pub fn dif(f1: &Fraction, f2: &Fraction) -> Fraction {
+    let numerator = f1.get_numerator()*f2.get_denominator()-f2.get_numerator()*f1.get_denominator();
+    let denominator = f1.get_denominator()*f2.get_denominator();
+    Fraction::new(numerator, denominator)
+}
+
+pub fn mult(f1: &Fraction, f2: &Fraction) -> Fraction {
+    let numerator = f1.get_numerator()*f2.get_numerator();
+    let denominator = f1.get_denominator()*f2.get_denominator();
+    Fraction::new(numerator, denominator)
+}
+
+pub fn div(f1: &Fraction, f2: &Fraction) -> Fraction {
+    let numerator = f1.get_numerator()*f2.get_denominator();
+    let denominator = f1.get_denominator()*f2.get_numerator();
+    Fraction::new(numerator, denominator)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod fraction;
+
+use fraction::Fraction;
 use std::io::{self, Write};
 
 fn main() {
@@ -16,10 +19,10 @@ fn main() {
     }
     println!("Enter one by one {} equations", n);
     temp = String::new();
-    let mut _matrix: Vec<Vec<f64>> = Vec::new();
+    let mut _matrix: Vec<Vec<Fraction>> = Vec::new();
 
     for _i in 0..n {
-        let mut _vec_temp: Vec<f64> = Vec::new();
+        let mut _vec_temp: Vec<Fraction> = Vec::new();
         loop {
             io::stdin().read_line(&mut temp).ok().expect("error");
             if temp.split(" ").collect::<Vec<&str>>().len() > n {
@@ -32,7 +35,8 @@ fn main() {
         }
         for j in temp.split(" ").collect::<Vec<&str>>() {
             let _conv: f64 = j.trim().parse().unwrap();
-            _vec_temp.push(_conv);
+            let frac_temp = Fraction::new(_conv, 1 as f64);
+            _vec_temp.push(frac_temp);
         }
         _matrix.push(_vec_temp);
         temp = String::new();
@@ -44,32 +48,37 @@ fn main() {
     print_vec_solution(_solution);
 }
 
-fn print_matrix(_matrix: &Vec<Vec<f64>>) {
+fn print_matrix(_matrix: &Vec<Vec<Fraction>>) {
     for _i in _matrix.iter() {
         print!("\n|\t");
         for _j in 0.._i.len() {
             if _i.len() - 1 == _j {
                 print!(":\t");
             }
-            print!("{}\t", _i[_j]);
+            print!("{}/{}\t", _i[_j].get_numerator(), _i[_j].get_denominator());
         }
         println!("|");
     }
 }
 
-fn gauss_jordan(mut _matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+fn gauss_jordan(mut _matrix: Vec<Vec<Fraction>>) -> Vec<Vec<Fraction>> {
     for _it in 0.._matrix.len() {
         _matrix = find_pibot(_it, _matrix);
-        if _matrix[_it][_it] == 0 as f64 {
+        if _matrix[_it][_it].value() == 0 as f64 {
             println!("It is not possible to solve");
         } else {
-            for _i in 0.._matrix.len() {
+            for _i in _it.._matrix.len() {
                 if _i != _it {
-                    let _multiplier = _matrix[_it][_it] / _matrix[_i][_it];
+                    let _multiplier = fraction::div(&_matrix[_it][_it], &_matrix[_i][_it]);
                     for _k in 0.._matrix[_i].len() {
-                        _matrix[_i][_k] = _matrix[_it][_k] - _matrix[_i][_k]*_multiplier;
+                        _matrix[_i][_k] = fraction::dif(
+                            &_matrix[_it][_k],
+                            &fraction::mult(&_matrix[_i][_k], &_multiplier),
+                        );
                     }
                 }
+                print_matrix(&_matrix);
+                println!("\n--------------------------------------------");
             }
         }
     }
@@ -77,10 +86,10 @@ fn gauss_jordan(mut _matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     _matrix
 }
 
-fn find_pibot(_idx: usize, mut _matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-    if _matrix[_idx][_idx] == 0 as f64 {
+fn find_pibot(_idx: usize, mut _matrix: Vec<Vec<Fraction>>) -> Vec<Vec<Fraction>> {
+    if _matrix[_idx][_idx].value() == 0 as f64 {
         for _i in _idx.._matrix.len() {
-            if _matrix[_i][_idx] == 0 as f64 {
+            if _matrix[_i][_idx].value() == 0 as f64 {
                 continue;
             } else {
                 _matrix.swap(_idx, _i);
@@ -91,30 +100,39 @@ fn find_pibot(_idx: usize, mut _matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     _matrix
 }
 
-fn find_solutions (_matrix: Vec<Vec<f64>>) ->Vec<f64>{
-    let mut temp: Vec<f64> = Vec::new();
-
-    for _i in 0.._matrix.len(){
-        println!("{}",_matrix[_i][_matrix.len()-1]);
-        let x_i = _matrix[_i][_i] / _matrix[_i][_matrix[_i].len()-1];
+fn find_solutions(_matrix: Vec<Vec<Fraction>>) -> Vec<Fraction> {
+    let mut temp: Vec<Fraction> = Vec::new();
+    for _i in 0.._matrix.len() {
+        let mut temp_frac = Fraction::new(0.0, 1.0);
+        println!("Inicia it");
+        for _j in _matrix.len() - _i - 1.._matrix.len() {
+            if _j == _matrix.len() - _i - 1 {
+                println!("Esto es lo que suma {}/{}",temp_frac.get_numerator(),temp_frac.get_denominator());
+                temp_frac = fraction::sum(&temp_frac, &_matrix[_matrix.len()-_i-1][_j]);
+            }else {
+                temp_frac = fraction::sum(&temp_frac, &fraction::mult(&temp[_matrix.len()-1-_j],&_matrix[_matrix.len()-_i-1][_j]));
+                println!("Esto es lo que suma {}/{}",temp_frac.get_numerator(),temp_frac.get_denominator());
+            }
+            
+        }
+        println!("Fin IT");
+        println!("Esto es lo que divide num {}/{}",temp_frac.get_numerator(),temp_frac.get_denominator());
+        println!("Esto es lo que divide deno {}/{}",&_matrix[_matrix.len()-_i-1][_matrix[_i].len() - 1].get_numerator(),&_matrix[_matrix.len()-_i-1][_matrix[_i].len() - 1].get_denominator());
+        let x_i = fraction::div(&_matrix[_matrix.len()-_i-1][_matrix[_i].len() - 1],&temp_frac);
+        println!("lo que se guarda {}/{}",x_i.get_numerator(),x_i.get_denominator());
         temp.push(x_i);
     }
-    for _i in temp.iter(){
-        print!("{} ",_i);
-    }
-    println!("");
     temp
 }
 
-
-fn print_vec_solution (_vec: Vec<f64>){
+fn print_vec_solution(_vec: Vec<Fraction>) {
     let mut out = String::new();
-    for _i in 0.._vec.len(){
+    for _i in 0.._vec.len() {
         out.push_str("x_");
         out.push_str(&_i.to_string());
         out.push_str("=");
-        out.push_str(&_vec[_i].to_string());
-        out.push_str(", ");
+        out.push_str(&_vec[_i].value().to_string());
+        out.push_str(" ");
     }
-    println!("[ {} ]",out);
+    println!("[ {} ]", out);
 }
